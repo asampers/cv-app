e3371571c0f824af5c00b9ea5174250c340f7cf4
nanarama <annasampers@gmail.com>
2023-08-24 15:49:24 -0500

    Add Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-    <div className='resume container-fluid d-flex align-items-center flex-column border rounded border-black'>
+    <div className='resume container-fluid d-flex align-items-center flex-column border border-black border-2 rounded '>
       <h1>Resume</h1>
     </div>
   )
