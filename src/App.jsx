ffc0ff9083249e040021be7d3d7b217caa52bd12
nanarama <annasampers@gmail.com>
2023-08-24 17:22:35 -0500

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 //import { useState } from 'react'
 import './App.css'
-
+import { Header } from './components/Header'
 function App() {
 
   return (
     <div className='resume container-fluid d-flex align-items-center flex-column border border-black border-2 rounded'>
       <h1>Resume</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+export function Header() {
+  return (
+    <>
+    <h2>Header</h2>
+    <form>
+      <div className="d-flex flex-column">
+        <div className="d-flex">
+          <div className="form-floating mb-3 me-2">
+            <input type="text" className="form-control" placeholder="Jane"/>
+            <label>First Name</label>
+          </div> 
+          <div className="form-floating mb-3 me-2">
+            <input type="text" className="form-control" placeholder="Smith"/>
+            <label>Last Name</label>
+          </div>
+          <div className="form-floating mb-3 me-2">
+            <input type="tel" className="form-control" placeholder="123-456-7899"/>
+            <label>Phone</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input type="text" className="form-control" placeholder="NYC"/>
+            <label>Location</label>
+          </div>
+        </div>
+        <div className="d-flex">
+          <div className="form-floating flex-fill mb-3 me-2">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="name@example.com"
+            />
+            <label>Email address</label>
+          </div>
+          <div className="form-floating mb-3 me-2">
+            <input type="text" className="form-control" placeholder="LinkedIn"/>
+            <label>LinkedIn</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input type="text" className="form-control" placeholder="Portfolio"/>
+            <label>Portfolio</label>
+          </div>
+          <button type="submit" className="btn btn-primary align-self-center ms-4">
+            Submit
+          </button>
+        </div> 
+      </div> 
+    </form>
+    </>
+  )
+}
