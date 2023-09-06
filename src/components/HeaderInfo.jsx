4a39a2f0bd620e7f8acb39f3c8b109d81fc72115
nanarama <annasampers@gmail.com>
2023-09-06 11:11:35 -0500

    Adjust spacing for HeaderInfo

--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export function HeaderInfo({person}) {
   return (
-    <div className='d-flex align-items-center'>
+    <div className='d-flex align-items-center justify-content-evenly flex-wrap'>
       {person.email && <p className='me-3'><ion-icon name="mail-outline"></ion-icon>{person.email}</p> }
       {person.phone && <p className='me-3'><ion-icon name="call-outline"></ion-icon>{person.phone}</p> }
       {person.location && <p className='me-3'><ion-icon name="location-outline"></ion-icon>{person.location}</p> }
